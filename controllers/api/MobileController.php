dd53dd76c1e9a8a8bf188c9c80f26c1b45c5cb58
hadiPc <fayadhadi2014@gmail.com>
2022-03-09 00:33:17 +0200

    Merge origin/main into main
    Conflicts:
    	controllers/api/MobileController.php

--- controllers/api/MobileController.php
+++ controllers/api/MobileController.php
@@ -811,7 +811,7 @@ WHERE roomId =" . $item["id"];
              FROM rooms
              JOIN users ON rooms.r_admin = users.id
              LEFT JOIN followrooms ON followrooms.r_room = rooms.id AND followrooms.r_user = $userId
-             WHERE  rooms.r_admin = $userId ;";
+             WHERE  rooms.r_admin = $userId    ORDER BY rooms.creation_date DESC ;";
 
         $command = Yii::$app->db->createCommand($sql);
         $arrayList = $command->queryAll();
